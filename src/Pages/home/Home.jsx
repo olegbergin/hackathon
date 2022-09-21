7f28240fdbb31563b1efa9f48cfbc2ebc295a901
yohaiprins <yohaiprins33@gmail.com>
2022-09-21 18:01:14 +0300

    update frontend

---
+++ src/Pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom"
+import './home.css';
+import React, {useState} from "react";
+import PastEventsCards from "../../components/home/past-events-cards";
+
+
+const Home = () => {
+    return(
+       
+<PastEventsCards/>
+    )
+}
+export default Home
+
+
+
