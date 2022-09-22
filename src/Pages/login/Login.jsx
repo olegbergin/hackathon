1e89eb7f703d4e565d8c947fde9a1dbc016abc20
ChimaCuma <oleg.bragin.01@gmail.com>
2022-09-22 08:40:46 +0300

    sort

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,26 +1,26 @@
 import { useForm } from "react-hook-form";
 import "./style.css";
 import * as yup from "yup";
-import React, {useState} from "react";
+import React from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import axios from 'axios';
 import jwt_decode from "jwt-decode";
 import { useDispatch } from "react-redux";
 import { updateRole } from "../../store/userSlice";
+import { updateToken } from "../../store/tokenSlice";
+import { updateEvent } from "../../store/eventSlice";
+import { updateContact } from "../../store/contactSlice";
+import { updateSocial } from "../../store/socialSlice";
 
 
 const schema = yup.object().shape({
   username: yup.string().required(),
-  password: yup.string().required().min(4,"4-12 symbols").max(12,"4-12 symbols"),
+  password: yup.string().required().min(4, "4-12 symbols").max(12, "4-12 symbols"),
 });
 
 export default function Form() {
-  const [token, setToken] = useState('')
-  const [role, setRole] = useState('')
-
   const dispatch = useDispatch();
 
-  
   const {
     register,
     formState: { errors, isValid },
@@ -29,20 +29,23 @@ export default function Form() {
   } = useForm({ mode: "all", resolver: yupResolver(schema) });
 
   const onSubmit = async (data) => {
-     axios.post('http://localhost:5000/auth/login', {"username":data.username,"password":data.password})
-    .then((res) => {
-      res.data && setToken(res.data.token)
-      var decoded = jwt_decode(res.data.token)
-      dispatch (updateRole(decoded.role))
-
-      setRole(decoded.role)
-      console.log(token);
-      console.log(role);
-    })
+    axios.post('http://localhost:5000/auth/login', { "username": data.username, "password": data.password })
+      .then((res) => {
+        dispatch(updateToken(res.data.token))
+        var decoded = jwt_decode(res.data.token)
+        dispatch(updateRole(decoded.role))
+        const token = res.data.token;
+        axios.get('http://localhost:5000/add/events', { headers: { 'Authorization': `Bearer ${token}` } })
+          .then(res => dispatch(updateEvent(res.data)))
+        axios.get('http://localhost:5000/add/social', { headers: { 'Authorization': `Bearer ${token}` } })
+          .then(res => dispatch(updateSocial(res.data)))
+        axios.get('http://localhost:5000/add/contact', { headers: { 'Authorization': `Bearer ${token}` } })
+          .then(res => dispatch(updateContact(res.data)))
+      })
     reset();
   };
 
-  
+
   return (
     <div className="Form">
       <h1>Log In</h1>
@@ -69,6 +72,6 @@ export default function Form() {
   );
 }
 
-      
+
 
 
--- src/components/home/past-events-cards.jsx
+++ src/components/home/past-events-cards.jsx
@@ -1,52 +1,55 @@
-import data from './past-events.json'
 import './past-events-cards.css';
-import React, {useState} from "react";
+import React  from "react";
 import Popup from './popup';
 import { useEffect } from 'react';
 import "aos/dist/aos.css";
 import Aos from "aos";
+import { useSelector } from 'react-redux';
 
 
-const PastEventsCards= () => {
-  useEffect(()=>{
-    Aos.init({duration:1000});
-},[]);
-    return(
-        <div>
-            <div className="header">
-           <p className='header-title' dir="rtl" data-aos="zoom-in">"בנגב ייבחן העם בישראל"</p>
-            <h6 dir="rtl" className='david-title' data-aos="zoom-in">דוד בן גוריון</h6>
+const PastEventsCards = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
+  const events = useSelector((state) => state.event.event);
+
+  return (
+    <div>
+      <div className="header">
+        <p className='header-title' dir="rtl" data-aos="zoom-in">"בנגב ייבחן העם בישראל"</p>
+        <h6 dir="rtl" className='david-title' data-aos="zoom-in">דוד בן גוריון</h6>
+      </div>
+      <div className="wellcome-title-container">
+        <p className='wellcome-title' dir="rtl">ברוכים הבאים לאתר עובדים עריית דימונה!</p>
+      </div>
+      <div className='past-events-title-container'>
+        <p className='past-events-title' dir="rtl">מאירועי השבוע האחרון שלנו:</p>
+      </div>
+      <div className="past-events-cards" data-aos="flip-up">
+
+      {events.slice(0,4).map(({_id, image, name, description, date}) => {
+          return (
+            <div className="cards-div-past-event" key={_id}>
+              <div className="template-past-evnt" >
+                <div className="img-div-past-evnt">
+                  <img className="image-past-events" src={image} alt="" />
+                </div>
+                <h3>{name}</h3>
+                <p className="desctiption">{description}</p>
+                <p className="phone">{date}</p>
+                <Popup />
+
+              </div>
             </div>
-            <div className="wellcome-title-container">
-            <p className='wellcome-title' dir="rtl">ברוכים הבאים לאתר עובדים עריית דימונה!</p>
-            </div>
-            <div className='past-events-title-container'>
-            <p className='past-events-title' dir="rtl">מאירועי השבוע האחרון שלנו:</p>
-            </div>
-            <div className="past-events-cards" data-aos="flip-up">
-                
-            {  data.map((val) => {
-                      return(
-                        <div className="cards-div-past-event" >
-                        <div className="template-past-evnt" key={val.id}>
-                          <div className="img-div-past-evnt">
-                            <img className="image-past-events" src={val.image} alt="" />
-                          </div>
-                             <h3>{val.name}</h3>
-                             <p className="desctiption">{val.description}</p>
-                             <p className="phone">{val.date}</p>
-                             <Popup/>
-                             
-                        </div> 
-                        </div>
-                       
-                      )
-                      })       
-                    }      
-            </div>
-        </div>
-        
-      
-    )
+
+          )
+        })
+        }
+      </div>
+    </div>
+
+
+  )
 }
 export default PastEventsCards
