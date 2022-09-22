da83207a5a5893c5ed7b11eb13a9c06e67abd7e7
ChimaCuma <oleg.bragin.01@gmail.com>
2022-09-22 09:11:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
             <Route path="/Home" element={<LogIn />} />
             <Route path="/Benifits" element={<LogIn />} />
             <Route path="/Contacts" element={<LogIn />} />
+            <Route path="/Profile" element={<LogIn/>} />
           </Routes>
         </div>
       </>
