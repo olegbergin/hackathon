e8089388cc38d74c09a9349decf5530f6445d3c0
ChimaCuma <oleg.bragin.01@gmail.com>
2022-09-22 10:32:55 +0300

    update

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -4,26 +4,26 @@ import FutureEventsCards from "../../components/home/future-events-cards";
 import SocialMediaCards from "../../components/home/social-media-cards";
 import Fotter from '../../components/fotter';
 import Chat from '../../components/messag/Chat';
+import { useSelector } from 'react-redux';
 
 
 
 const Home = () => {
-    return(
-       <div>
-<PastEventsCards/>
+    const role = useSelector((state) => state.role.role);
 
-<FutureEventsCards/>
+    return (
+        <div>
+            <PastEventsCards />
 
-<Chat/>
+            <FutureEventsCards />
+            {(role=="ADMIN"||role=="SUPERADMIN")? <Chat /> :<div/>}
+
+            <SocialMediaCards />
+
+            <Fotter />
 
 
-
-<SocialMediaCards/>
-
-<Fotter/>
-
-
-    </div>
+        </div>
 
     )
 }
--- src/components/messag/Chat.jsx
+++ src/components/messag/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './Chat.css';
 import { useState } from 'react';
 import Aos from "aos";
@@ -10,57 +10,57 @@ import "aos/dist/aos.css";
 
 
 function Chat() {
-  const[name,setName]=useState([])
-  const[temp,setTemp]=useState("")
- function handleSubmit()
- {
-    let tempname=[...name];
+  const [name, setName] = useState([])
+  const [temp, setTemp] = useState("")
+  function handleSubmit() {
+    let tempname = [...name];
     tempname.push(temp)
     setName(tempname);
- }
+  }
 
 
- useEffect(()=>{
-  Aos.init({duration:1000});
-},[]);
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
   return (
+
     <div>
-    
-        <h1 dir="rtl" className='chat-title' data-aos="zoom-in">הודעות:</h1>
-        <div className='todo-app'>
+
+      <h1 dir="rtl" className='chat-title' data-aos="zoom-in">הודעות:</h1>
+      <div className='todo-app'>
         <ul className='chat-ul'>
-      {name.map((element,index) => (
-      <div className='chat-div1'> 
-        <h1 className='chat-username1'></h1>
-        <h3 key={index} className="h3-chat1"> {name[index]} </h3>
-        <img src="" className='chat-img1' alt="" />
+          {name.map((element, index) => (
+            <div className='chat-div1'>
+              <h1 className='chat-username1'></h1>
+              <h3 key={index} className="h3-chat1"> {name[index]} </h3>
+              <img src="" className='chat-img1' alt="" />
+            </div>
+
+          ))}
+          <div className='chat-div'>
+            <h1 className='chat-username'>dan</h1>
+            <h3 className="h3-chat">hello world</h3>
+            <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" className='chat-img' alt="" />
+          </div>
+
+          <div className='chat-div'>
+            <h1 className='chat-username'>jon</h1>
+            <h3 className="h3-chat">hello world</h3>
+            <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" className='chat-img' alt="" />
+          </div>
+
+          <div className='chat-div'>
+            <h1 className='chat-username'>ben</h1>
+            <h3 className="h3-chat">hello world</h3>
+            <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" className='chat-img' alt="" />
+          </div>
+        </ul>
       </div>
-
-      ))}
-        <div className='chat-div'>
-        <h1 className='chat-username'>dan</h1>
-          <h3 className="h3-chat">hello world</h3>
-          <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" className='chat-img' alt="" />
-        </div>
-
-        <div className='chat-div'>
-        <h1 className='chat-username'>jon</h1>
-          <h3 className="h3-chat">hello world</h3>
-          <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" className='chat-img' alt="" />
-        </div>
-
-        <div className='chat-div'>
-        <h1 className='chat-username'>ben</h1>
-          <h3 className="h3-chat">hello world</h3>
-          <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" className='chat-img' alt="" />
-        </div>
-     </ul>
-     </div>
-     <div className='input-container'>
-      <input className='chat-input' type='text' onChange={(e)=>setTemp(e.target.value)} />
-     <button onClick={handleSubmit} className="chat-btn">submit</button>
-     </div>
+      <div className='input-container'>
+        <input className='chat-input' type='text' onChange={(e) => setTemp(e.target.value)} />
+        <button onClick={handleSubmit} className="chat-btn">submit</button>
+      </div>
 
     </div>
   );
